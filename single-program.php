685e45524dc9495f291dbe313b7e5e257f9c5e8a
Josh Rathke <josh.rathke@ywammontana.org>
2015-01-19 12:16:01 -0700

    More Work on Program Pages
    More work on program pages, mainly the addition of program info and
    schedule sections.

--- single-program.php
+++ single-program.php
@@ -13,7 +13,7 @@
  
  
  <div class="row">
- 	<div class="medium-9 columns single-program-content-container">
+ 	<div class="large-9 columns single-program-content-container">
  		<?php while (have_posts()) : the_post(); ?>
  			
  			<?php
@@ -24,7 +24,6 @@
 			 */
 			
 			$program_id = $post->ID;
-			print_r($program_id);
 			$program_object = new programInfo($program_id);
 			
 			print_r($program_object);
@@ -43,6 +42,28 @@
 		<?php endwhile;?>
 		
 		
+		<?php
+		/**
+		 * Program Info Section
+		 * This is a section that displays general information
+		 * about the program, such as how long the program lasts,
+		 * if there is an outreach how long it is, and the 
+		 * accreditation information.
+		 */
+		 ?>
+		 
+		 <div class="row program-info-container">
+		 	<div class="medium-3 columns program-info-duration-container">
+		 		<div class="program-info-duration-number"><?php echo $program_object->academic_info['program_duration']; ?></div>
+		 		<div class="program-info-duration-desc">Weeks Total</div>
+		 	</div>
+		 	<div class="medium-9 columns program-info-academic-container">
+		 		<h5>Accreditation <i class="fa fa-graduation-cap"></i></h5>
+		 		<?php echo $program_object->academic_info['accreditation']; ?>
+		 	</div>
+		 </div>
+		
+		
 		
 		<?php
 		/**
@@ -52,31 +73,61 @@
 		 * 	apply for each of the scheduled schools. 
 		 */
 
-		echo '<ul class="medium-block-grid-3 program-dates-container">';
+		echo '<ul class="small-block-grid-1 medium-block-grid-3 program-dates-container">';
 			if (!empty($program_object->schedule)) {
+					
+				// Dispaly available program instaces	
 				foreach ($program_object->schedule as $program_occurance) {
 					echo '<li><div class="program-date-info-container">';
-					echo '<h3>' . $program_occurance['quarter'] . '</h3>';
+					echo '<h4>' . $program_occurance['quarter'] . '</h4>';
 						echo '<ul>';
-							echo '<li>' . $program_occurance['start_date'] . '</li>';
-							echo '<li>' . $program_occurance['end_date'] . '</li>';
-							echo '<li>' . $program_occurance['total_cost'] . '</li>';
-							echo '<li>' . $program_occurance['app_deadline'] . '</li>';
+						
+							$program_info_string = '<li>%s:<span class="right">%s</span></li>';
+							
+							echo sprintf($program_info_string, 'Start Date', date('m/d/y', strtotime($program_occurance['start_date'])));
+							echo sprintf($program_info_string, 'End Date', date('m/d/y', strtotime($program_occurance['end_date'])));
+							echo sprintf($program_info_string, 'Total Cost', $program_occurance['total_cost']);
+							echo sprintf($program_info_string, 'Apps Due', date('m/d/y', strtotime($program_occurance['app_deadline'])));
 						echo '</ul>';
 						echo '<a href="#_" class="button">Apply Online</a>';
 					echo '</div></li>';
-				}
+					}
+				
 			} else {
 				echo "Sorry there aren't any available dates at this time.";
 			}
 		echo '</ul>';
+		
+		// Display some of the academic and application requirements ?>
+		<div class="row program-prereqs-container alert-box warning">
+			<div class="small-3 medium-2 columns program-prereqs-icon-container">
+				<i class="fa fa-check-square-o"></i>
+			</div>
+			<div class="small-9 medium-10 columns">
+				<div class="program-pre-reqs">
+					<h5>Pre-Requisites</h5>
+					<?php echo $program_object->academic_info['recommended_prereqs']; ?>
+				</div>
+			</div>
+		</div>
+		
+		
+		
+		<?php 
+		/**
+		 * Video Section
+		 * This is the section of each school page where we pull in
+		 * any videos that may lend themselves to the school.
+		 */
+		
+		
 		
 		?>
 		
 		
  	</div>
  	
- 	<div class="medium-3 columns">
+ 	<div class="large-3 columns">
  		
  	</div>
  	
