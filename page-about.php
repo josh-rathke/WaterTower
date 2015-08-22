5518488b6743a550582b731ad45a663cb84f0c97
Josh Rathke <josh.rathke@ywammontana.org>
2015-08-22 17:29:32 -0600

    About Us Bugfix

--- page-about.php
+++ page-about.php
@@ -140,31 +140,26 @@ get_header(); ?>
         
     </div>
 
-</div>
+    <?php 
+    /**
+     *	Check for type of sidebar to display on pages,
+     *	then display the correct sidebar.
+     */
 
+    if ( rwmb_meta('page_sidebar') == 'headings') { ?>
 
-<?php 
-/**
- *	Check for type of sidebar to display on pages,
- *	then display the correct sidebar.
- */
-
-if ( rwmb_meta('page_sidebar') == 'headings') { ?>
-
-    <div class="large-3 columns stick-to-parent-side-nav">
-        <div class="magellan-container" data-magellan-expedition>
-          <dl class="sub-nav side-nav-container side-nav-by-heading">
-          </dl>
+        <div class="large-3 columns stick-to-parent-side-nav">
+            <div class="magellan-container" data-magellan-expedition>
+              <dl class="sub-nav side-nav-container side-nav-by-heading">
+              </dl>
+            </div>
         </div>
-    </div>
 
-<?php } else { ?>
-    <div class="large-4 columns">
-        <?php get_sidebar(); ?>
-    </div>
-<?php }?>
-
-
-
-
+    <?php } else { ?>
+        <div class="large-4 columns">
+            <?php get_sidebar(); ?>
+        </div>
+    <?php }?>
+    
+</div>
 <?php get_footer(); ?>
