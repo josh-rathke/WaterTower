16fb8629fc8954647b5846ef3560802f4669179a
Josh Rathke <josh.rathke@ywammontana.org>
2015-10-14 03:53:15 -0600

    Killed Zopim Chat

--- footer.php
+++ footer.php
@@ -96,14 +96,16 @@ endif; ?>
 
 <!--Start of Zopim Live Chat Script-->
 <script type="text/javascript">
+    /*
 window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
 d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
 _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
 $.src="//v2.zopim.com/?36NGmoT0awHc6akseNLUaHnYhTOLopEN";z.t=+new Date;$.
 type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+ */
 </script>
 <!--End of Zopim Live Chat Script-->
-<a href="#" class="chat-button activate-zopim"><i class="fa fa-comments-o"></i> Chat With Us</a>
+<!-- <a href="#" class="chat-button activate-zopim"><i class="fa fa-comments-o"></i> Chat With Us</a> -->
 
 
 <?php wp_footer(); ?>
