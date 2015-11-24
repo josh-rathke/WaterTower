a3e71c1116afde3d5d63b90172cea78a24521e3e
Josh Rathke <josh.rathke@ywammontana.org>
2015-11-24 07:47:05 -0700

    Bug fix for Promote Post Shortcake Output

--- parts/promote_post.php
+++ parts/promote_post.php
@@ -6,7 +6,7 @@
         </div>
         <div class="columns medium-7">
             <a href="<?php echo get_permalink($post_id); ?>"><h4><?php echo $title; ?></h4></a>
-            <p><?php echo $description ? $description : get_the_excerpt($post_id); ?></p>
+            <p><?php echo $description ? $description : get_excerpt_by_id($post_id); ?></p>
         </div>
     </div>
 </div>
