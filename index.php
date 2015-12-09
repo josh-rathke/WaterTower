c2a15d673bcc66e3418791f56c7763b5089819a7
Josh Rathke <josh.rathke@ywammontana.org>
2015-12-08 20:06:18 -0700

    Initial FoundationPress 6 Dump

--- index.php
+++ index.php
@@ -1,54 +1,46 @@
 <?php
-
 /**
- *    Blog Template
- *    This displays all of the different blog views for
- *    the site.
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * Learn more: {@link https://codex.wordpress.org/Template_Hierarchy}
+ *
+ * @package WordPress
+ * @subpackage FoundationPress
+ * @since FoundationPress 1.0.0
  */
 
-$page_id = get_id_by_slug( 'blog' );
+get_header(); ?>
 
-get_header();
-?>
-<div class="row content-archive-container">
-	<div class="small-12 large-8 columns" role="main">
-		
-    <?php get_template_part( 'parts/archive_header' ); ?>
+<div id="page" role="main">
+	<article class="main-content">
+	<?php if ( have_posts() ) : ?>
 
-    <?php if ( have_posts() ) : ?>
+		<?php /* Start the Loop */ ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'content', get_post_format() ); ?>
+		<?php endwhile; ?>
 
-    <?php do_action( 'watertower_before_content' ); ?>
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
 
-    <?php while ( have_posts() ) : the_post(); ?>
-    <?php get_template_part( 'content' ); ?>
-    <?php
-endwhile; ?>
+		<?php endif; // End have_posts() check. ?>
 
-    <?php else : ?>
-    <?php get_template_part( 'content', 'none' ); ?>
+		<?php /* Display navigation to next/previous pages when applicable */ ?>
+		<?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
+			<nav id="post-nav">
+				<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
+				<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
+			</nav>
+		<?php } ?>
 
-    <?php do_action( 'watertower_before_pagination' ); ?>
+	</article>
+	<?php get_sidebar(); ?>
 
-    <?php
-endif;?>
-
-
-
-    <?php if ( function_exists( 'watertower_pagination' ) ) { watertower_pagination();
-} else if ( is_paged() ) { ?>
-		<nav id="post-nav">
-			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'watertower' ) ); ?></div>
-			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'watertower' ) ); ?></div>
-		</nav>
-    <?php
-} ?>
-
-    <?php do_action( 'watertower_after_content' ); ?>
-
-	</div>
-	
-	<aside id="sidebar" class="small-12 large-4 columns stick-to-parent">
-    <?php get_sidebar(); ?>
-	</aside>
 </div>
+
 <?php get_footer(); ?>
